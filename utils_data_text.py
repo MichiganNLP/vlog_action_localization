dda5d6fd3a3bbd2e96d7cebbed13dffc49e55fcf
Oana Ignat <oignat@umich.edu>
2020-01-29 19:35:39 -0500

    lal

--- utils_data_text.py
+++ utils_data_text.py
@@ -1650,13 +1650,13 @@ def get_features_from_data(train_data, val_data, test_data):
     # data_clips_train = get_visual_features_from_data(data_clips_train)
     data_actions_emb_train = [i[1] for i in data_actions_train]
     data_actions_names_train = [i[0] for i in data_actions_train]
-    data_actions_names_train = [i[0] for i in data_actions_train]
+    data_clips_names_train = [i[0] for i in data_actions_train]
 
     data_clips_feat_val = np.asarray([i[1] for i in data_clips_val])
     # data_clips_val = get_visual_features_from_data(data_clips_val)
     data_actions_emb_val = [i[1] for i in data_actions_val]
     data_actions_names_val = [i[0] for i in data_actions_val]
-    data_actions_names_val = [i[0] for i in data_actions_val]
+    data_clips_names_val = [i[0] for i in data_actions_val]
 
     data_clips_feat_test = np.asarray([i[1] for i in data_clips_test])
     data_clips_names_test = [i[0] for i in data_clips_test]
