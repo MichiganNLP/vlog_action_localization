7f53778b78c49c7274964d426045bcf514752940
Oana Ignat <oignat@umich.edu>
2020-03-01 17:14:35 -0500

    lala

--- utils_data_text.py
+++ utils_data_text.py
@@ -2202,13 +2202,11 @@ def add_object_features(type):
     else:
         print("Error argument object label type")
 
-    wheel_feature = np.zeros((1, 2048))
-    for clip in dict_FasterRCNN_features_clips.keys():
-        # list_labels = read_class_results(clip)
-        if clip == "6p1_5mini_3_001":
-            list_features = dict_FasterRCNN_features_clips[clip]
-            wheel_feature = np.array(list_features[0])
-            break
+    # for clip in dict_FasterRCNN_features_clips.keys():
+    #     # list_labels = read_class_results(clip)
+    #     if clip == "6p1_5mini_3_001":
+    #         list_features = dict_FasterRCNN_features_clips[clip]
+    #         whe
 
 
     dict_clip_features = {}
@@ -2216,12 +2214,11 @@ def add_object_features(type):
         # list_labels = read_class_results(clip)
         list_features = dict_FasterRCNN_features_clips[clip]
         if not list_features:
-            dict_clip_features[clip] = np.zeros((1, 2048))
+            dict_clip_features[clip] = np.zeros(2048)
             continue
-        sum_label_embeddings = np.zeros((1, 2048))
+        sum_label_embeddings = np.zeros(2048)
         for feature in list_features:
-            if not np.array_equal(feature, wheel_feature):
-                sum_label_embeddings += feature
+            sum_label_embeddings += np.array(feature)
 
         result = np.array(sum_label_embeddings) / len(list_features)  # 2. avg
         dict_clip_features[clip] = np.squeeze(result)
