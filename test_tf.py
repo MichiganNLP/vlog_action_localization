f0d2e22543748c5bb9c9397bd773fbee09c5b209
Oana Ignat <oignat@umich.edu>
2020-03-03 15:22:32 -0500

    l

--- test_tf.py
+++ test_tf.py
@@ -80,7 +80,8 @@ def method_tf_actions(train_data, val_data, test_data):
     # inputs_words are just a list of sentences (i.e. ['the sky is blue', 'someone cutting an apple'])
     input_words = tf.placeholder(tf.string, shape=(None,))
 
-    module = hub.Module("https://tfhub.dev/deepmind/mil-nce/s3d/1")
+    # module = hub.Module("https://tfhub.dev/deepmind/mil-nce/s3d/1")
+    module = hub.Module("https://tfhub.dev/deepmind/mil-nce/i3d/1")
 
     vision_output = module(input_frames, signature='video', as_dict=True)
     text_output = module(input_words, signature='text', as_dict=True)
